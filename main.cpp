1aaec1e7b023718ad8eee3679bb736bba1c83aa8
Max Yurchneko <maxonus2@gmail.com>
2018-09-03 20:18:21 +0300

    Added .gitignore

--- main.cpp
+++ main.cpp
@@ -7,20 +7,24 @@
 #include "Coordinates.h"
 #include "Figures/King.h"
 #include "Figures/Pawn.h"
+#include "Figures/Knight.h"
+#include "Figures/Castle.h"
+#include "Figures/Bishop.h"
+#include "Figures/Queen.h"
+
 
 using namespace std;
 
 
 int main (){
 
-    // TODO: проверить работу get_possible_moves()
-    Coordinates coordinates(7, 7);
-    Pawn *w_pawn = new Pawn(false, coordinates);
-    w_pawn->calculate_available_moves();
-    w_pawn->print_available_moves();
+    Coordinates w_queen_coordinates(3 , 3);
+
+    Queen *w_queen = new Queen(true, w_queen_coordinates);
+
+    w_queen->calculate_available_moves();
+    w_queen->print_available_moves();
 
 
-    cout << endl;
-    system("pause");
-    return 0;
+
 };
