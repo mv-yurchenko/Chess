dab20e6c6e10a73a3e0c512f3f75c06be761b129
Max Yurchneko <maxonus2@gmail.com>
2018-09-03 20:16:58 +0300

    Fixed possible moves calculating if figure is black

--- Figures/Bishop.cpp
+++ Figures/Bishop.cpp
@@ -140,7 +140,7 @@ void Bishop::calculate_possible_moves(std::vector<Figure> white_figures, std::ve
             }
         }
 
-        // Проверим по Y вниз
+        // bottom left
         new_coordinates.setX(getCoordinates().getX());
         new_coordinates.setY(getCoordinates().getY());
         is_way_free = true;
@@ -167,12 +167,10 @@ void Bishop::calculate_possible_moves(std::vector<Figure> white_figures, std::ve
     }
 
     if (is_figure_black()){
-
-        //  Проверяем по X вправо
-
         for(uint8_t i = 1; i < 8; i++) {
             if (is_way_free and is_not_out_of_border) {
                 new_coordinates.setX(getCoordinates().getX() + i);
+                new_coordinates.setY(getCoordinates().getY() + i);
                 if (is_out_of_border(new_coordinates)) {
                     is_not_out_of_border = false;
                 } else {
@@ -188,37 +186,15 @@ void Bishop::calculate_possible_moves(std::vector<Figure> white_figures, std::ve
             }
         }
 
-        // Проверим по X влево
+        //  top left
         new_coordinates.setX(getCoordinates().getX());
+        new_coordinates.setY(getCoordinates().getY());
         is_way_free = true;
         is_not_out_of_border = true;
 
         for(uint8_t i = 1; i < 8; i++) {
             if (is_way_free and is_not_out_of_border) {
                 new_coordinates.setX(getCoordinates().getX() - i);
-                if (is_out_of_border(new_coordinates)) {
-                    is_not_out_of_border = false;
-                } else {
-                    if (is_cell_busy_by_enemy_figure(new_coordinates, white_figures)){
-                        add_move_to_possible_moves(new_coordinates);
-                        is_way_free = false;
-                    }
-                    if (is_cell_busy_by_ally_figure(new_coordinates, black_figures)){
-                        is_way_free = false;
-                    }
-                    add_move_to_possible_moves(new_coordinates);
-                }
-            }
-        }
-
-        // Проверим по Y вверх
-        new_coordinates.setX(getCoordinates().getX());
-        new_coordinates.setX(getCoordinates().getY());
-        is_way_free = true;
-        is_not_out_of_border = true;
-
-        for(uint8_t i = 1; i < 8; i++) {
-            if (is_way_free and is_not_out_of_border) {
                 new_coordinates.setX(getCoordinates().getY() + i);
                 if (is_out_of_border(new_coordinates)) {
                     is_not_out_of_border = false;
@@ -235,14 +211,41 @@ void Bishop::calculate_possible_moves(std::vector<Figure> white_figures, std::ve
             }
         }
 
-        // Проверим по Y вниз
-        new_coordinates.setX(getCoordinates().getY());
+        // bottom right
+        new_coordinates.setX(getCoordinates().getX());
+        new_coordinates.setY(getCoordinates().getY());
         is_way_free = true;
         is_not_out_of_border = true;
 
         for(uint8_t i = 1; i < 8; i++) {
             if (is_way_free and is_not_out_of_border) {
-                new_coordinates.setX(getCoordinates().getY() - i);
+                new_coordinates.setX(getCoordinates().getX() + i);
+                new_coordinates.setY(getCoordinates().getY() - i);
+                if (is_out_of_border(new_coordinates)) {
+                    is_not_out_of_border = false;
+                } else {
+                    if (is_cell_busy_by_enemy_figure(new_coordinates, white_figures)){
+                        add_move_to_possible_moves(new_coordinates);
+                        is_way_free = false;
+                    }
+                    if (is_cell_busy_by_ally_figure(new_coordinates, black_figures)){
+                        is_way_free = false;
+                    }
+                    add_move_to_possible_moves(new_coordinates);
+                }
+            }
+        }
+
+        // bottom left
+        new_coordinates.setX(getCoordinates().getX());
+        new_coordinates.setY(getCoordinates().getY());
+        is_way_free = true;
+        is_not_out_of_border = true;
+
+        for(uint8_t i = 1; i < 8; i++) {
+            if (is_way_free and is_not_out_of_border) {
+                new_coordinates.setY(getCoordinates().getY() - i);
+                new_coordinates.setX(getCoordinates().getX() - i);
                 if (is_out_of_border(new_coordinates)) {
                     is_not_out_of_border = false;
                 } else {
@@ -258,7 +261,6 @@ void Bishop::calculate_possible_moves(std::vector<Figure> white_figures, std::ve
             }
         }
     }
-
 }
 
 
