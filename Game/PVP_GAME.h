11aab39c53a4d3a05278e9a13e7862f00ea36952
Unknown <joker0071911@gmail.com>
2018-09-05 22:20:37 +0300

    WIP PVP_CLASS

--- Game/PVP_GAME.h
+++ Game/PVP_GAME.h
@@ -16,6 +16,8 @@ public:
 
     void pvp_game(){
 
+        initialize_game();
+
         this->player1_side = true;
         this->player2_side = false;
 
@@ -32,9 +34,31 @@ public:
 
                 int new_x = input_coordinate("x");
                 int new_y = input_coordinate("y");
+
+                if (getCurrent_desk()->get_figure_by_coordinates(x, y)->getSide()) {
+                    move_figure(x, y, new_x, new_y);
+                } else{
+                    std::cout<<"Move is not possible" << std::endl;
+                }
             }
             else{
+                std::cout<<"Input figure coordinates" << std::endl;
 
+                int x = input_coordinate("x");
+                int y = input_coordinate("y");
+
+                std::cout<<"Your choice:" << getCurrent_desk()->get_figure_by_coordinates(x, y)->getName()<< std::endl;
+
+                std::cout<<"Input new coordinates" << std::endl;
+
+                int new_x = input_coordinate("x");
+                int new_y = input_coordinate("y");
+
+                if (not getCurrent_desk()->get_figure_by_coordinates(x, y)->getSide()) {
+                    move_figure(x, y, new_x, new_y);
+                } else{
+                    std::cout<<"Move is not possible" << std::endl;
+                }
             }
         }
     }
