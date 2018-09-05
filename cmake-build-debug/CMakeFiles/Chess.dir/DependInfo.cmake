d3e629afdf80993de6a777c5796d8139936518ee
Max Yurchneko <maxonus2@gmail.com>
2018-09-05 17:16:42 +0300

    Added CMake

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "P:/Chess/Coordinates.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Coordinates.cpp.obj"
+  "P:/Chess/Desk.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Desk.cpp.obj"
+  "P:/Chess/Figures/Bishop.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Bishop.cpp.obj"
+  "P:/Chess/Figures/Castle.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Castle.cpp.obj"
+  "P:/Chess/Figures/Figure.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Figure.cpp.obj"
+  "P:/Chess/Figures/King.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/King.cpp.obj"
+  "P:/Chess/Figures/Knight.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Knight.cpp.obj"
+  "P:/Chess/Figures/Pawn.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Pawn.cpp.obj"
+  "P:/Chess/Figures/Queen.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Figures/Queen.cpp.obj"
+  "P:/Chess/Move.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Move.cpp.obj"
+  "P:/Chess/main.cpp" "P:/Chess/cmake-build-debug/CMakeFiles/Chess.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
