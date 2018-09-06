d5cf67d1284f83f6d8c153567d5d4a54e5399420
Max Yurchneko <maxonus2@gmail.com>
2018-09-07 00:11:50 +0300

    renamed class to FiguresPossibleMovesWriter

--- LogsWriter/FiguresPossibleMovesWriter.cpp
+++ LogsWriter/FiguresPossibleMovesWriter.cpp
@@ -2,14 +2,14 @@
 // Created by Max Yurchenko on 06.09.2018.
 //
 
-#include "FiguresPossibleMovesWirter.h"
+#include "FiguresPossibleMovesWriter.h"
 
-void FiguresPossibleMovesWirter::write_all_figures_possible_moves(std::vector<Figure> white_figures,
+void FiguresPossibleMovesWriter::write_all_figures_possible_moves(std::vector<Figure> white_figures,
                                                                   std::vector<Figure> black_figures) {
     // Общий вид логов : 1 строчка - текущие координаты, остальные - Possible_moves
     for(const auto &figure : white_figures){
         // LogsFiles/Figures/Black/figureName.txt
-        this->full_path_to_output_file = path_to_white_figure_logs_directory +  figure.getName() + ".txt";
+        this->full_path_to_output_file = this->path_to_white_figure_logs_directory +  figure.getName() + ".txt";
 
         std::ofstream output_file;
         output_file.open(this->full_path_to_output_file);
@@ -19,7 +19,7 @@ void FiguresPossibleMovesWirter::write_all_figures_possible_moves(std::vector<Fi
 
     for(const auto &figure : black_figures){
         // LogsFiles/Figures/Black/figureName.txt
-        this->full_path_to_output_file = path_to_white_figure_logs_directory +  figure.getName() + ".txt";
+        this->full_path_to_output_file = this->path_to_black_figure_logs_directory +  figure.getName() + ".txt";
 
         std::ofstream output_file;
         output_file.open(this->full_path_to_output_file);
@@ -28,7 +28,7 @@ void FiguresPossibleMovesWirter::write_all_figures_possible_moves(std::vector<Fi
     }
 }
 
-FiguresPossibleMovesWirter::FiguresPossibleMovesWirter() {
+FiguresPossibleMovesWriter::FiguresPossibleMovesWriter() {
     this->path_to_white_figure_logs_directory = "LogsFiles/Figures/White/";
     this->path_to_black_figure_logs_directory = "LogsFiles/Figures/Black/";
 }
--- LogsWriter/FiguresPossibleMovesWriter.h
+++ LogsWriter/FiguresPossibleMovesWriter.h
@@ -2,21 +2,21 @@
 // Created by Max Yurchenko on 06.09.2018.
 //
 
-#ifndef CHESS_FIGURESPOSSIBLEMOVESWIRTER_H
-#define CHESS_FIGURESPOSSIBLEMOVESWIRTER_H
+#ifndef CHESS_FIGURESPOSSIBLEMOVESWRITER_H
+#define CHESS_FIGURESPOSSIBLEMOVESWRITER_H
 
 #include "../Figures/Figure.h"
 #include "fstream"
 
-class FiguresPossibleMovesWirter {
+class FiguresPossibleMovesWriter {
     std::string path_to_white_figure_logs_directory;
     std::string path_to_black_figure_logs_directory;
     std::string full_path_to_output_file;
 public:
-    FiguresPossibleMovesWirter();
+    FiguresPossibleMovesWriter();
 
     void write_all_figures_possible_moves(std::vector<Figure> white_figures, std::vector<Figure> black_figures);
 };
 
 
-#endif //CHESS_FIGURESPOSSIBLEMOVESWIRTER_H
+#endif //CHESS_FIGURESPOSSIBLEMOVESWRITER_H
