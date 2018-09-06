9e3e3d8a57562987a1f41cb7fb830a1a90328466
Max Yurchneko <maxonus2@gmail.com>
2018-09-07 00:08:59 +0300

    Started outpyt function

--- LogsWriter/FiguresPossibleMovesWirter.h
+++ LogsWriter/FiguresPossibleMovesWirter.h
@@ -5,9 +5,17 @@
 #ifndef CHESS_FIGURESPOSSIBLEMOVESWIRTER_H
 #define CHESS_FIGURESPOSSIBLEMOVESWIRTER_H
 
+#include "../Figures/Figure.h"
+#include "fstream"
 
 class FiguresPossibleMovesWirter {
+    std::string path_to_white_figure_logs_directory;
+    std::string path_to_black_figure_logs_directory;
+    std::string full_path_to_output_file;
+public:
+    FiguresPossibleMovesWirter();
 
+    void write_all_figures_possible_moves(std::vector<Figure> white_figures, std::vector<Figure> black_figures);
 };
 
 
