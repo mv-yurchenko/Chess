3f5764c9efdb6b4b58984c94b002cfb9d07792c2
Maxim Yurchenko <joker0071911@gmail.com>
2018-09-08 11:17:18 +0300

    Started GamesLogsWriter

---
+++ LogsWriter/GameLogsWriter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Max Yurchenko on 08.09.2018.
+//
+
+#include "GameLogsWriter.h"
---
+++ LogsWriter/GameLogsWriter.h
@@ -0,0 +1,14 @@
+//
+// Created by Max Yurchenko on 08.09.2018.
+//
+
+#ifndef CHESS_GAMELOGSWRITER_H
+#define CHESS_GAMELOGSWRITER_H
+
+
+class GameLogsWriter {
+
+};
+
+
+#endif //CHESS_GAMELOGSWRITER_H
