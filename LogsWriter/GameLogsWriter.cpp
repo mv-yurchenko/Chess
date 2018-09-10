81c757983a7acfba08662802146958e89f54adcd
Max Yurchneko <maxonus2@gmail.com>
2018-09-10 22:43:32 +0300

    added write_current_desk

--- LogsWriter/GameLogsWriter.cpp
+++ LogsWriter/GameLogsWriter.cpp
@@ -8,8 +8,19 @@ GameLogsWriter::GameLogsWriter() {
     this->log_filename = "GameLogs";
     this->log_file_path = "../LogsFiles/" + this->log_filename+ ".txt";
     this->log_file.open(this->log_file_path);
+    this->desk_log_filename = "Desk";
+    this->desk_log_file_path = "../LogsFiles/" + this->desk_log_filename+ ".txt";
+    this->desk_log_file.open(this->desk_log_file_path);
 }
 
 void GameLogsWriter::write_log_about_move(Figure *figure, int old_x, int old_y, int new_x, int new_y) {
     log_file << figure->get_side_as_string() << " " << figure->getName() << " was moved from : (" << old_x << " , " << old_y << ") to (" << new_x << " , " << new_y << ")"<< std::endl;
+}
+
+void GameLogsWriter::write_current_desk(Desk *desk) {
+    for (int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            desk_log_file << std::to_string(j) << std::to_string(i) << " " << desk->get_figure_by_coordinates(j, i)->getName() << " " << desk->get_figure_by_coordinates(j, i)->get_side_as_string()<< std::endl;
+        }
+    }
 }
--- LogsWriter/GameLogsWriter.h
+++ LogsWriter/GameLogsWriter.h
@@ -14,10 +14,15 @@ class GameLogsWriter {
     std::string log_file_path;
     std::string log_filename;
     std::ofstream log_file;
+    std::ofstream desk_log_file;
+    std::string desk_log_filename;
+    std::string desk_log_file_path;
 public:
     GameLogsWriter();
 
     void write_log_about_move(Figure *figure, int old_x, int old_y, int new_x, int new_y);
+
+    void write_current_desk(Desk *desk);
 };
 
 
