366fbac68c62d592b51691c06618ead30fc526aa
Maxim Yurchenko <joker0071911@gmail.com>
2018-09-13 08:53:44 +0300

    Added "save game funtion"

--- LogsWriter/GameLogsWriter.h
+++ LogsWriter/GameLogsWriter.h
@@ -17,12 +17,17 @@ class GameLogsWriter {
     std::ofstream desk_log_file;
     std::string desk_log_filename;
     std::string desk_log_file_path;
+    std::string game_save_filename;
+    std::string game_save_file_path;
+    std::ofstream game_save_file;
 public:
     GameLogsWriter();
 
     void write_log_about_move(Figure *figure, int old_x, int old_y, int new_x, int new_y);
 
-    void write_current_desk(Desk *desk);
+    void write_current_desk(Desk *desk, bool to_save_game);
+
+    void save_game(Desk *desk, bool is_white_turn);
 };
 
 
