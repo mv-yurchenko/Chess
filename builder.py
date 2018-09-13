8a5748d734ab55fdcfcc2893a9892e90c0a38094
Maxim Yurchenko <joker0071911@gmail.com>
2018-09-13 16:27:53 +0300

    added builder

---
+++ builder.py
@@ -0,0 +1,20 @@
+import os
+
+def add_file(command, filename):
+    return command + " " + str(filename)
+
+os_command = "g++ main.cpp"
+
+os_command = add_file(os_command, "Game/*.cpp")
+
+os_command = add_file(os_command, "Figures/*.cpp")
+
+os_command = add_file(os_command, "LogsWriter/*.cpp")
+
+os_command = add_file(os_command, "Desk.cpp")
+
+os_command = add_file(os_command, "Move.cpp")
+
+os_command = add_file(os_command, "Coordinates.cpp")
+
+os.system(os_command)
