4f6f9c7f1505f02e1703addaa6804b1e551168a4
Max Yurchneko <maxonus2@gmail.com>
2018-09-18 23:51:53 +0300

    Minor refactoring

--- Figures/King.cpp
+++ Figures/King.cpp
@@ -49,8 +49,6 @@ void King::calculate_one_direction(const std::vector<Figure> &ally_figures, bool
     if (left){
         new_coordinates.setX(getCoordinates().getX() - 1);
     }
-    new_coordinates.print_coordinates();
-    std::cout << is_out_of_border(new_coordinates) << " : " << is_cell_busy_by_ally_figure(new_coordinates, ally_figures) << std::endl;
     if (not is_out_of_border(new_coordinates) and not is_cell_busy_by_ally_figure(new_coordinates, ally_figures)) {
         add_move_to_possible_moves(new_coordinates);
     }
