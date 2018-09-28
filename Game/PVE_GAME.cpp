32d524451b1b5ad58f363294e6b995d9912aef55
Maxim Yurchenko <joker0071911@gmail.com>
2018-09-28 09:08:21 +0300

    Fixed Bug with changing Human side

--- Game/PVE_GAME.cpp
+++ Game/PVE_GAME.cpp
@@ -16,6 +16,7 @@ void PVE_GAME::engine_turn(bool is_white_turn) {
     Move engine_move = engine.move();
     if (this->getCurrent_desk()->move_figure(engine_move.getOld_coordinates(), engine_move.getNew_coordinates())){
         this->setWhite_turn(not is_white_turn);
+        this->setIs_game_finished(is_mate(engine_move.getNew_coordinates().getX(), engine_move.getNew_coordinates().getY()));
     }
 }
 
@@ -30,7 +31,6 @@ void PVE_GAME::pve_game() {
         chessGraphic.print_desk(getCurrent_desk());
         if (is_player_turn){
             human_turn(isWhite_turn());
-            is_player_turn = not is_player_turn;
         } else{
             engine_turn(isWhite_turn());
             is_player_turn = not is_player_turn;
@@ -40,4 +40,27 @@ void PVE_GAME::pve_game() {
 
 void PVE_GAME::write_desk() {
     gameLogsWriter.write_current_desk(getCurrent_desk(), false);
+}
+
+void PVE_GAME::human_turn(bool is_white_turn) {
+    std::cout << "Input figure to move" << std::endl;
+    Coordinates figure_coordinates = input_coordinates();
+    print_msg_about_figure_choice(figure_coordinates);
+
+    std::cout << "Input new coordinates" << std::endl;
+    Coordinates new_coordinates = input_coordinates();
+
+    if (player_turn(isWhite_turn(), figure_coordinates, new_coordinates)) {
+        print_msg_about_success_move(figure_coordinates.getX(), figure_coordinates.getY(),
+                                     new_coordinates.getX(), new_coordinates.getY());
+        setWhite_turn(not isWhite_turn());
+        this->setIs_game_finished(is_mate(new_coordinates.getX(), new_coordinates.getY()));
+        this->is_player_turn = not this->is_player_turn;
+    } else {
+        getCurrent_desk()->get_figure_by_coordinates(figure_coordinates)->print_possible_moves();
+        print_msg_about_failed_move(figure_coordinates.getX(), figure_coordinates.getY(),
+                                    new_coordinates.getX(), new_coordinates.getY());
+    }
+    write_desk();
+    figuresPossibleMovesWriter.write_all_figures_possible_moves(getCurrent_desk());
 }
--- Game/PVE_GAME.h
+++ Game/PVE_GAME.h
@@ -26,27 +26,7 @@ public:
 
     void pve_game();
 
-    void human_turn(bool is_white_turn){
-        std::cout << "Input figure to move" << std::endl;
-        Coordinates figure_coordinates = input_coordinates();
-        print_msg_about_figure_choice(figure_coordinates);
-
-        std::cout << "Input new coordinates" << std::endl;
-        Coordinates new_coordinates = input_coordinates();
-
-        if (player_turn(isWhite_turn(), figure_coordinates, new_coordinates)) {
-            print_msg_about_success_move(figure_coordinates.getX(), figure_coordinates.getY(),
-                                         new_coordinates.getX(), new_coordinates.getY());
-            setWhite_turn(not isWhite_turn());
-            this->setIs_game_finished(is_mate(new_coordinates.getX(), new_coordinates.getY()));
-        } else {
-            getCurrent_desk()->get_figure_by_coordinates(figure_coordinates)->print_possible_moves();
-            print_msg_about_failed_move(figure_coordinates.getX(), figure_coordinates.getY(),
-                                        new_coordinates.getX(), new_coordinates.getY());
-        }
-        write_desk();
-        figuresPossibleMovesWriter.write_all_figures_possible_moves(getCurrent_desk());
-    }
+    void human_turn(bool is_white_turn);
 
     void engine_turn(bool side);
 };
