523a99a9d6f3e0ce8fab42ecca49d425c7bb7e6a
Maxim Yurchenko <joker0071911@gmail.com>
2018-10-06 21:32:47 +0300

    WIP engine
    todo randomzer

--- main.cpp
+++ main.cpp
@@ -3,23 +3,7 @@
 
 
 int main(){
+    PVE_GAME pve_game;
 
-    Desk desk;
-
-    desk.initialize_desk();
-
-    Coordinates old_coordinates(4, 0);
-    Coordinates new_coordinates(4, 4);
-
-    desk.force_move_figure(old_coordinates, new_coordinates);
-    std::cout << desk.get_figure_by_coordinates(new_coordinates)->getName();
-    desk.get_figure_by_coordinates(new_coordinates)->print_possible_moves();
-
-//    PVE_GAME pve_game;
-//
-//    pve_game.initialize_game();
-//
-//    Engine engine(pve_game.getCurrent_desk(), pve_game.isWhite_turn());
-//
-//    engine.getMost_profitable_moves();
+    pve_game.pve_game();
 }
